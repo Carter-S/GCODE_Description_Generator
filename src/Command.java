a72e7ddb017ab415a296331adf334118d4180f81
Carter Springall <carter.springall01@gmail.com>
2021-01-20 15:33:09 +0000

    updating with new case

--- src/Command.java
+++ src/Command.java
@@ -36,28 +36,35 @@ public class Command {
     public String commandDescription(){
         StringBuilder cmdDesc = new StringBuilder();
         for (String character : commandCodes.keySet()){
-            if(character.equals("G")){
-                switch (commandCodes.get(character)) {
-                    case ("0") -> cmdDesc.append("Rapid linear movement to point: ");
-                    case ("1") -> cmdDesc.append("Feed Linear movement to point: ");
-                    case ("21") -> cmdDesc.append("MM mode");
-                    case ("90") -> cmdDesc.append("Absolute coordinate positioning");
-                }
-            }
-            if(character.equals("M")){
-                switch (commandCodes.get(character)) {
-                    case ("6") -> cmdDesc.append("Tool change command");
-                    case ("30") -> cmdDesc.append("Program finished");
-                }
-            }
-            if(character.equals("T")){
-                cmdDesc.append(" to tool " + commandCodes.get(character));
-            }
-            if(character.equals("X")|| character.equals("Y") || character.equals("Z")){
-                cmdDesc.append(character + "=" + commandCodes.get(character) + " ");
-            }
-            if(character.equals("F")){
-                cmdDesc.append("| Feed rate=" + commandCodes.get(character) + " ");
+            switch (character) {
+                case "G":
+                    switch (commandCodes.get(character)) {
+                        case ("0") -> cmdDesc.append("Rapid linear movement to point: ");
+                        case ("1") -> cmdDesc.append("Feed Linear movement to point: ");
+                        case ("4") -> cmdDesc.append("Dwell time");
+                        case ("21") -> cmdDesc.append("MM mode");
+                        case ("90") -> cmdDesc.append("Absolute coordinate positioning");
+                    }
+                    break;
+                case "M":
+                    switch (commandCodes.get(character)) {
+                        case ("6") -> cmdDesc.append("Tool change command");
+                        case ("30") -> cmdDesc.append("Program finished");
+                    }
+                    break;
+                case "T":
+                    cmdDesc.append(" to tool " + commandCodes.get(character));
+                    break;
+                case "X":
+                case "Y":
+                case "Z":
+                    cmdDesc.append(character + "=" + commandCodes.get(character) + " ");
+                    break;
+                case "F":
+                    cmdDesc.append("| Feed rate=" + commandCodes.get(character) + " ");
+                    break;
+                case "P":
+                    cmdDesc.append(" for " + commandCodes.get(character));
             }
         }
         return cmdDesc.toString();
